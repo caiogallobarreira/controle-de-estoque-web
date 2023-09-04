5a7bac7b0ad3aa691ef91203b813f9cf1cfdbfa2
caiogallobarreira <caiogallobarreira@gmail.com>
2023-09-04 19:09:46 -0300

    * fetch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,39 +3,33 @@ import PageTitle from '@/components/PageTitle'
 import ProductCard from '@/components/ProductCard'
 import Image from 'next/image'
 
-const produtos = [
-    {
-      id: 1,
-      estoqueId: 1,
-      nome: "Faca de Cozinha",
-      descricao: "Facas de cozinha com Lâminas em Aço Inox e Cabos de Polipropileno Preto",
-      imagemUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
-      quantidade: 100,
-      quantidadeMinima: 10,
-  },
-  {
-      id: 2,
-      estoqueId: 1,
-      nome: "Televisão",
-      descricao: "Televisão 4K de 55 polegadas",
-      imagemUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
-      quantidade: 50,
-      quantidadeMinima: 5,
-  },
-];
+async function getProdutos() {
+  const url = 'http://localhost:8080/api/v1/produto';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    console.log(response);
+    return response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
 
 
-export default function Home() {
+export default async function Home() {
+  let produtos = await getProdutos();
+  produtos = produtos._embedded.entityModelList;
   return (
     <>
       <Navbar />
       <div className="flex flex-col rounded-md m-5 p-10 justify-items-center bg-slate-200">
         <PageTitle title="Dashboard" />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nihil. Quasi aperiam dolore ab repellendus porro soluta quisquam. Assumenda quas a tempore provident enim illum numquam amet, quibusdam culpa fugiat?</p>
-        <div className='grid grid-flow-col self-center gap-4 my-10'>
-          {produtos.map((produto) =>
-            <ProductCard key={produto.id} produto={produto} />
-          )}
+        <div className='grid grid-flow-col grid-rows-2 self-center gap-4 my-10'>
+          {produtos.map(produto => <ProductCard key={produto.id} produto={produto}></ProductCard>) }
         </div>
       </div>
     </>
